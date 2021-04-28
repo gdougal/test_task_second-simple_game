414f3839ef051fba303c9a62bd64b650c18acf00
gdougal <gdougal@student.21-school.ru>
2021-04-28 17:57:55 +0300

    done

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,33 +4,44 @@
 
 
 
-int	game_cycle() {
-	logic						game("srcs/config.txt");
-	wrap_window			window(game.getResourses().getWinResourse());
-	game.setSessionWindow(window.getPubWindow().get());
-	while (window.getPubWindow()->isOpen()) {
-		sf::Event e;
-		while (window.getPubWindow()->pollEvent(e)) {
-			switch (e.type) {
-				case sf::Event::EventType::Closed:
-					window.getPubWindow()->close();
-					break;
-				case e.KeyPressed:
-					game.handler_key(e.key.code);
-					break;
-				default:
-					break;
+int	game_cycle(const std::string	conf_dir = std::string("./config.txt")) {
+	try {
+		logic game(conf_dir);
+		wrap_window window(game.getResourses().getWinResourse());
+		game.setSessionWindow(window.getPubWindow().get());
+		while (window.getPubWindow()->isOpen()) {
+			sf::Event e;
+			while (window.getPubWindow()->pollEvent(e)) {
+				switch (e.type) {
+					case sf::Event::EventType::Closed:
+						window.getPubWindow()->close();
+						break;
+					case e.KeyPressed:
+						game.handler_key(e.key.code);
+						break;
+					default:
+						break;
+				}
 			}
-		}
 			game.game_actions();
+		}
+		return 0;
 	}
-	return 0;
+	catch (...) {
+		return 1;
+	}
 }
 
 
-int main() {
+int main(int argc, char** argv) {
 	srand(time(nullptr));
-	game_cycle();
-	std::cout << "Hello, World!" << std::endl;
+	int ret = 0;
+	if (argc == 1)
+		ret = game_cycle();
+	else if (argc > 1)
+		ret = game_cycle(argv[1]);
+	if (ret) {
+		std::cout << "oups, some trouble... maybe config path?" << std::endl;
+	}
 	return 0;
 }
